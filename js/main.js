36f7af036c5b94e1eaeba8f6321bdb7cc75627da
Todor Panev <hi@panev.net>
2015-05-15 17:06:11 +0300

    initial revamp - header

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 $(document).ready( function () {
-    $('p.first-p').addClass('first-ani');
-
-    $('p.second-p').addClass('second-ani');
-
-    $('form').addClass('third-ani');
-});
 
 $(function () {
     $('#subForm').submit(function (e) {
